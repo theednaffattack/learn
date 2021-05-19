58f1668e5dd9e16afc9eb26953da24063d65b801
Eddie Naff <eddie@eddienaff.dev>
2021-05-18 17:43:44 -0700

    Add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Learn React Stuff</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>Audio Player</main>
+    </div>
+  );
+}
